d7c52d76423985e9f51a803ccf9bcc70a7e9c143
mmancip <Martial.Mancip@MaisondelaSimulatipn.fr>
2020-11-23 12:09:48 +0100

    Improve DISPLAY detection.

--- webrtcconnect/get_DISPLAY.sh
+++ webrtcconnect/get_DISPLAY.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -u ${USER} -o pid= | xargs -I{} cat /proc/{}/environ 2>/dev/null |  tr '\0' '\n' |  grep -m1 '^DISPLAY=' |tee -a ./out_DISPLAY
+pgrep "(.*session.*|.*terminal.*|.*onsole.*)" | xargs -I{} cat /proc/{}/environ 2>/dev/null | tr '\0' '\n' | grep -m1 '^DISPLAY=' |tee -a ./out_DISPLAY
