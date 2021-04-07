48f550bd83955d0bd3b8884bec1ec5fcd54a1a3a
mmancip <Martial.Mancip@MaisondelaSimulatipn.fr>
2021-04-07 18:40:18 +0200

    No more pactl call in hub launch_sound : done in job.

--- webrtcconnect/DockerHub/build_files/launch_sound.sh
+++ webrtcconnect/DockerHub/build_files/launch_sound.sh
@@ -8,23 +8,25 @@ DIR_PULSE=${HOME}/.config/pulse
 mkdir -p ${DIR_PULSE}
 LOCAL_CONF=${DIR_PULSE}/client.conf
 
+#pulseaudio --daemonize=no &
+
 # All pulseaudio in container listen to 4000
 echo 'default-server = tcp:localhost:4000' > ${LOCAL_CONF}
 
 ssh -4 -fNT -i ${HOME}/.ssh/id_rsa_$IP -L4000:$NATIVE $LOGIN@$IP &
 
-sleep 2
+# sleep 2
 
-export dev_source=$(pactl info | sed -En 's/Default Source: (.*)/\1/p')
-export dev_sink=$(pactl info | sed -En 's/Default Sink: (.*)/\1/p')
+# export dev_source=$(pactl info | sed -En 's/Default Source: (.*)/\1/p')
+# export dev_sink=$(pactl info | sed -En 's/Default Sink: (.*)/\1/p')
 
-pactl load-module module-null-sink sink_name=stu_source sink_properties=device.description="stu_source" &> /dev/null
-pactl load-module module-null-sink sink_name=stu_sink sink_properties=device.description="stu_sink" &> /dev/null
+# pactl load-module module-null-sink sink_name=stu_source sink_properties=device.description="stu_source" &> /dev/null
+# pactl load-module module-null-sink sink_name=stu_sink sink_properties=device.description="stu_sink" &> /dev/null
 
-sleep 2
+# sleep 2
 
-pactl load-module module-loopback source=stu_source.monitor sink=${dev_sink} &> /dev/null
-pactl load-module module-loopback source=stu_source.monitor sink=stu_sink &> /dev/null
-pactl load-module module-loopback source=${dev_source} sink=stu_sink &> /dev/null
+# pactl load-module module-loopback source=stu_source.monitor sink=${dev_sink} &> /dev/null
+# pactl load-module module-loopback source=stu_source.monitor sink=stu_sink &> /dev/null
+# pactl load-module module-loopback source=${dev_source} sink=stu_sink &> /dev/null
 
 
